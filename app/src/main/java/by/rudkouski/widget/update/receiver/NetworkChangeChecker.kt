0634e5168fa38a37b259c7b9dd0ffe843d60c21e
arudkouski <andrei.rudkouski@gmail.com>
2019-09-27 12:46:15 +0300

    Changed checking for internet availability

--- app/src/main/java/by/rudkouski/widget/update/receiver/NetworkChangeChecker.kt
+++ app/src/main/java/by/rudkouski/widget/update/receiver/NetworkChangeChecker.kt
@@ -10,16 +10,22 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import java.lang.Exception
-import java.net.URL
+import java.net.InetSocketAddress
+import java.net.Socket
+
 
 object NetworkChangeChecker {
+
+    private const val HOST_NAME = "8.8.8.8"
+    private const val DNS_PORT = 53
+    private const val TIMEOUT_MILLIS = 1500
 
     private val observers = HashSet<NetworkObserver>()
     private val connectivityManager = appContext.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
     private val networkRequest = NetworkRequest.Builder().build()
     private val networkCallbacks = object : ConnectivityManager.NetworkCallback() {
         override fun onAvailable(network: Network) {
-            if (isInternetAvailable(network)) {
+            if (isInternetAvailable()) {
                 observers.forEach { it.startUpdate(appContext) }
                 unregisterNetworkChangeReceiver()
             }
@@ -40,24 +46,23 @@ object NetworkChangeChecker {
 
     fun isOnline(): Boolean {
         val activeNetwork = connectivityManager.activeNetwork
-        return isInternetAvailable(activeNetwork)
+        return activeNetwork != null && isInternetAvailable()
     }
 
-    private fun isInternetAvailable(network: Network?): Boolean {
-        if (network != null) {
-            return runBlocking {
-                withContext(Dispatchers.IO) {
-                    try {
-                        val connection = network.openConnection(URL("https://www.google.com"))
-                        connection.connect()
-                        true
-                    } catch (ex: Exception) {
-                        false
-                    }
+    private fun isInternetAvailable(): Boolean {
+        return runBlocking {
+            withContext(Dispatchers.IO) {
+                try {
+                    val sockAddress = InetSocketAddress(HOST_NAME, DNS_PORT)
+                    val sock = Socket()
+                    sock.connect(sockAddress, TIMEOUT_MILLIS)
+                    sock.close()
+                    true
+                } catch (ex: Exception) {
+                    false
                 }
             }
         }
-        return false
     }
 
     interface NetworkObserver {
